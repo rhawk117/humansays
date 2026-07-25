8440b2e2569353e7b3c13d3cece6515e38261888
rhawk117 <hawkinsry19@gmail.com>
2026-07-25 13:39:29 -0400

    chore(ops): setup repo pyproject stuff

---
+++ src/askance/__main__.py
@@ -0,0 +1,3 @@
+from askance.entrypoint import run_tool
+
+raise SystemExit(run_tool())
---
+++ src/askance/entrypoint.py
@@ -0,0 +1,11 @@
+
+
+
+
+def run_tool() -> int:
+    print('placeholder')
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(run_tool())
